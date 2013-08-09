9cd90c61cd342abc862ef60bb85c4b954bc72c9f
Lance Velasco <lance@mlvgroup.com>
2013-08-09 16:27:13 -0700

    Initial Commit

---
+++ TapMe/MLVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MLVAppDelegate.h
+//  TapMe
+//
+//  Created by Lance Velasco on 8/9/13.
+//  Copyright (c) 2013 MLV Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TapMe/MLVViewController.h
@@ -0,0 +1,13 @@
+//
+//  MLVViewController.h
+//  TapMe
+//
+//  Created by Lance Velasco on 8/9/13.
+//  Copyright (c) 2013 MLV Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLVViewController : UIViewController
+
+@end
